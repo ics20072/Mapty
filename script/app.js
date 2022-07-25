9b6f7231826ff031f550ee65a8a56bcb2da80088
MINAS\User <ics20072@uom.edu.gr>
2022-07-25 03:58:07 +0300

    Break code into modules

--- script/app.js
+++ script/app.js
@@ -1,62 +1,9 @@
 "use strict";
 
 import "../node_modules/leaflet/dist/leaflet.js";
-
-class Workout {
-  date = new Date();
-  id = (Date.now() + "").slice(-10);
-  clicks = 0;
-
-  constructor(coords, distance, duration) {
-    this.coords = coords; // [lat, lng]
-    this.distance = distance; // in km
-    this.duration = duration; // in min
-  }
-
-  _setDescription() {
-    const months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-
-    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
-  }
-
-  click() {
-    this.clicks++;
-  }
-}
-
-class Running extends Workout {
-  type = "running";
-
-  constructor(coords, distance, duration, cadence) {
-    super(coords, distance, duration);
-    this.cadence = cadence;
-    this.calcPace();
-    this._setDescription();
-  }
-
-  calcPace() {
-    // min/km
-    this.pace = this.duration / this.distance;
-    return this.pace;
-  }
-}
-
-class Cycling extends Workout {
-  type = "cycling";
-
-  constructor(coords, distance, duration, elevationGain) {
-    super(coords, distance, duration);
-    this.elevationGain = elevationGain;
-    this.calcSpeed();
-    this._setDescription();
-  }
-
-  calcSpeed() {
-    // km/h
-    this.speed = this.distance / (this.duration / 60);
-    return this.speed;
-  }
-}
+import Workout from "./workout.js";
+import Running from "./running.js";
+import Cycling from "./cycling.js";
 
 // APPLICATION ARCHITECTURE
 const form = document.querySelector(".form");
@@ -293,4 +240,4 @@ class App {
   }
 }
 
-const app = new App();
+new App();
